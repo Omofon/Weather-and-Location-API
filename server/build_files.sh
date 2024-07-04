7c0f482d5b5e6f7777a603fded63880b8c228bfd
Abasiomofon Udoh <114416186+Omofon@users.noreply.github.com>
2024-07-04 15:01:34 +0100

    Update build_files.sh

--- server/build_files.sh
+++ server/build_files.sh
@@ -2,9 +2,9 @@
 echo "Building the project..."
 python3.11 -m pip install -r requirements.txt
 
-echo "Make Migration..."
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+# echo "Make Migration..."
+# python3.11 manage.py makemigrations --noinput
+# python3.11 manage.py migrate --noinput
 
-echo "Collect Static..."
-python3.11 manage.py collectstatic --noinput --clear
+# echo "Collect Static..."
+# python3.11 manage.py collectstatic --noinput --clear
