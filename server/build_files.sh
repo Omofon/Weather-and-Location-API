8cbad355009754d33a6fba900f1ffdeef58c8ce2
Abasiomofon Udoh <abasiomofonudoh@gmail.com>
2024-07-04 12:48:12 +0100

    Updated build script for Vercel

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,3 +1,10 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Install pip for Python 3.9
+python3.9 -m ensurepip --upgrade
+
+# Install dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files (if you had any)
+# python3.9 manage.py collectstatic --noinput
--- server/server/settings.py
+++ server/server/settings.py
@@ -58,12 +58,8 @@ TEMPLATES = [
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME", "geo_ipserver"),
-        "USER": os.environ.get("DB_USER", "root"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", "3306"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
