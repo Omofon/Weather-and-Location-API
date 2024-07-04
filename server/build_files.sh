de308451d18725e4381ce5645548625228c9ffa8
Abasiomofon Udoh <abasiomofonudoh@gmail.com>
2024-07-04 12:33:36 +0100

    Updated build script for Vercel

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-python3.11 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Collect static files (if you had any)
 # python3.11 manage.py collectstatic --noinput
